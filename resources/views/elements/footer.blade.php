0d4a7acfda044e130ad208e5a60dbf63c7283def
nicoduflot <nico_duflot@yahoo.fr>
2016-01-19 16:11:56 +0100

    Ajout footer

--- resources/views/elements/footer.blade.php
+++ resources/views/elements/footer.blade.php
@@ -1,10 +1,59 @@
-@extends('layouts.front')
-
 @section('footer')
-
-<footer>
-
-
+<footer class="navbar navbar-default">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				RETROUVEZ NOUS AUSSI SUR :
+			</div>
+		</div>	
+		<div class="row">
+			<div class="col-lg-offset-3 col-lg-1">
+				<span class="glyphicon glyphicon-th-large"></span>
+			</div>
+			<div class="col-lg-1">
+				Teutter
+			</div>
+			<div class="col-lg-1">
+				géplus
+			</div>
+			<div class="col-lg-1">
+				pindereste
+			</div>
+			<div class="col-lg-1">
+				hein !	
+			</div>
+			<div class="col-lg-1">
+				iouteub
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-4">
+				NOS SERVICES<br /><br />
+				LIVRAISON<br />
+				FINANCEMENT<br />
+				CATALOGUES<br />
+				GARANTIES<br />
+				CONDITIONS GÉNÉRALE DE VENTE<br />
+			</div>
+			<div class="col-lg-offset-1 col-lg-4">
+				A PROPOS DE NOUS<br /><br />
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor 
+				incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+				exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute 
+				irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+				nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
+				officia deserunt mollit anim id est laborum.
+			</div>
+			<div class="col-lg-1">
+				LOGO
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-12">
+				CONTACT | POLITIQUE DE CONFIDENTIALITÉ | MENTIONS LÉGALES | PLAN DU SITE
+			</div>
+		</div>
+	</div>
 </footer>
 
 
