4a7e9ab383e625eb7d8723b002d7b5ffc72ad3bf
Loubna Fattouh <Loubna@MacBook-Pro-de-Loubna.local>
2016-01-19 15:30:55 +0100

    feuille de style

--- resources/views/pages/front.blade.php
+++ resources/views/pages/front.blade.php
@@ -7,19 +7,13 @@
 @include('elements.nav')
 
 @section('content')
-    <p>
+<p>
 
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer condimentum libero et tempor ornare. Ut interdum, est sed cursus mattis, sem ante lacinia dui, at aliquet mi magna vitae tortor. Donec non sapien ac risus eleifend varius. Etiam vitae dignissim est. Curabitur ornare mattis nisi at lobortis. Vestibulum at tortor varius, consectetur turpis sed, venenatis orci. Pellentesque sodales tortor ac leo ultrices, a auctor dui euismod. Quisque quis urna ac eros elementum cursus.
-
-Morbi ac auctor metus. Cras ac nulla tempus, aliquam elit eget, facilisis lorem. Aliquam ex velit, lacinia feugiat interdum et, suscipit non velit. Nulla tincidunt felis at lectus posuere congue. Fusce lobortis consectetur nisi, vel dignissim odio ornare ac. Nam cursus pretium tincidunt. Aenean in quam faucibus, lacinia quam quis, pharetra urna. Sed ac consequat felis, vitae aliquet ipsum. Curabitur sagittis bibendum tellus, ac rutrum lorem pretium rhoncus. In id luctus dolor. Phasellus semper tortor orci, eget efficitur neque tristique sed. Aliquam nec ante quis odio ornare molestie nec et diam. Phasellus non vehicula velit, quis porta neque. Suspendisse potenti. Morbi pulvinar justo sed hendrerit pellentesque.
-
-Maecenas semper eros sed tellus convallis lacinia. Vivamus placerat tincidunt purus vel mattis. Suspendisse eget pretium risus. Aliquam mattis sapien sit amet quam elementum, ut rhoncus justo mollis. Maecenas ornare auctor aliquet. Nunc quis dui sit amet ipsum vulputate suscipit. Pellentesque eget tempus sapien. Phasellus blandit posuere sem, id blandit turpis aliquet eu. Donec ac tempus diam. Vestibulum egestas, mi id sollicitudin lobortis, leo nisi maximus nisi, at maximus purus justo et diam. Donec purus orci, aliquam vitae venenatis in, maximus ut eros. Mauris vehicula sollicitudin mi, ac mattis lorem aliquet quis. Donec vel finibus erat, in congue justo.
-
-Nam volutpat, nunc et imperdiet viverra, purus erat condimentum enim, ac ullamcorper tortor ante sagittis velit. Maecenas volutpat lacus dui, nec efficitur augue iaculis cursus. Fusce varius, neque eu bibendum vehicula, tellus ante posuere neque, id tempus magna libero nec orci. Nulla gravida nisi non eros finibus, eu tincidunt eros iaculis. Aliquam quam quam, sollicitudin eget congue ut, faucibus eu quam. Proin sodales cursus elit vel mattis. Pellentesque ornare pretium bibendum. Morbi vel mi finibus, auctor augue ut, malesuada justo.
+Test
+Test
 
 
-
-    </p>
+</p>
 @endsection
 
 
