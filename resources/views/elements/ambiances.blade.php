b9aef07ad0cc992ca7351c73fe617d15e7fb5bd3
Loubna Fattouh <Loubna@MacBook-Pro-de-Loubna.local>
2016-01-20 16:50:23 +0100

    bloc ambiances

--- resources/views/elements/ambiances.blade.php
+++ resources/views/elements/ambiances.blade.php
@@ -2,14 +2,25 @@
 
 <div class="ambiance row">
     <div class="container">
-        <h1 class="title">Les ambiances
-        </h1>
+        <h1 class="title">Les ambiances</h1>
         <h3 class="sub-title">Chaque mois, on vous propose une sélection d'ambiance de la saison</h3>
+        <hr>
         <div class="content">
-            
-            
-            
+           <div class="row">
+                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 ambiance1">
+                    <img src="http://placehold.it/540x500" alt="">
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                    <div class="row ambiance2">
+                        <img src="http://placehold.it/540x240" alt="">
+                    </div>
+                    <div class="row ambiance3">
+                        <img src="http://placehold.it/540x240" alt="">
+                    </div>
+                </div>
+            </div>
         </div>
+        <h3 class="show-ambiances">Voir toutes les ambiances</h3>
     </div>  
 </div>
 
