fcf24d82a9d4f7d3fed7f786407a26874989dc67
Constance-L <constance.lemaire59000@gmail.com>
2016-01-20 11:12:15 +0100

    suppression dropdwon -accueil -ambiance

--- resources/views/elements/nav.blade.php
+++ resources/views/elements/nav.blade.php
@@ -2,7 +2,7 @@
 
 <nav class="navbar navbar-inverse navbar-fixed-top">
     <!-- Navbar Contents -->
-  <div class="container-fluid">
+  <div class="container">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
@@ -17,51 +17,10 @@
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <!--<li class="active"><a href="#">accueil<span class="sr-only">(current)</span></a></li>
-        <li><a href="#">creation</a></li>-->
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Accueil<span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li class="dropdown-header"><a href="#">sejours</a></li>
-            <li><a href="#">canape</a></li>
-            <li><a href="#">fauteuil</a></li>
-            <li><a href="#">table basse</a></li>
-            <li><a href="#">Meuble tv</a></li>
-            <li role="separator" class="divider"></li>
-            <li class="dropdown-header"><a href="#">Salle a manger</a></li>
-            <li><a href="#">chaises, tabourets</a></li>
-            <li><a href="#">bancs</a></li>
-            <li><a href="#">table de repas</a></li>
-            <li><a href="#">buffets, collones</a></li>
-            <li><a href="#">vaisseliers</a></li>
-            <li role="separator" class="divider"></li>
-            <li class="dropdown-header"><a href="#">chambre</a></li>
-            <li><a href="#">Lits</a></li>
-            <li><a href="#">Armoires</a></li>
-          </ul>
-        </li>
 
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">ambiance<span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li class="dropdown-header"><a href="#">sejours</a></li>
-            <li><a href="#">canape</a></li>
-            <li><a href="#">fauteuil</a></li>
-            <li><a href="#">table basse</a></li>
-            <li><a href="#">Meuble tv</a></li>
-            <li role="separator" class="divider"></li>
-            <li class="dropdown-header"><a href="#">Salle a manger</a></li>
-            <li><a href="#">chaises, tabourets</a></li>
-            <li><a href="#">bancs</a></li>
-            <li><a href="#">table de repas</a></li>
-            <li><a href="#">buffets, collones</a></li>
-            <li><a href="#">vaisseliers</a></li>
-            <li role="separator" class="divider"></li>
-            <li class="dropdown-header"><a href="#">chambre</a></li>
-            <li><a href="#">Lits</a></li>
-            <li><a href="#">Armoires</a></li>
-          </ul>
-        </li>
+        <li class="active"><a href="#">accueil<span class="sr-only">(current)</span></a></li>
+
+        <li><a href="#">ambiance</a></li>
 
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">cr&eacute;ation<span class="caret"></span></a>
