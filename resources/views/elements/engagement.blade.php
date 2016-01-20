19d5a980cb03daf89ab743ba562e70554ef40ebd
Loubna Fattouh <Loubna@MacBook-Pro-de-Loubna.local>
2016-01-20 10:24:42 +0100

    container engagement

---
+++ resources/views/elements/engagement.blade.php
@@ -0,0 +1,30 @@
+@section('engagement')
+
+<div class="engagement row">
+   <div class="container">
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+            <h3 class="small-title">14 jours pour changer d'avis</h3>
+            <div class="content">
+                <p>La couleur de votre fauteuil ne vous plait plus, l'armoire est trop grand ? Soyez serein, vous avez 14 jours pour changer d'avis</p>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+            <h3 class="small-title">Paiement sécurisé</h3>
+            <div class="content">
+                <p>Virement - Chèque - Paiement en plusieurs fois</p>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+            <h3 class="small-title">Moyen de paiement</h3>
+            <div class="content">
+                <i class="fa fa-cc-visa"></i>
+                <i class="fa fa-cc-paypal"></i>
+                <i class="fa fa-credit-card"></i>
+                <i class="fa fa-cc-mastercard"></i>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+@endsection
