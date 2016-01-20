1b941e7d2cd9c0ccc2d8f7950d5871378ba8a294
nicoduflot <nico_duflot@yahoo.fr>
2016-01-20 09:56:51 +0100

    footer

--- resources/views/elements/footer.blade.php
+++ resources/views/elements/footer.blade.php
@@ -1,5 +1,5 @@
 @section('footer')
-<footer class="navbar navbar-default">
+<footer>
 	<div class="container">
 		<div style="height:200px">
 				<!-- cette div est en attente de hauteur du header (deux doigts coupe faim) -->
@@ -29,33 +29,37 @@
 				<span class="glyphicon glyphicon-th-large"></span>
 			</div>
 		</div>
-		<div class="row">
+		<div class="row aPropos">
 			<div class="col-lg-3">
-				NOS SERVICES<br /><br />
-				LIVRAISON<br />
-				FINANCEMENT<br />
-				CATALOGUES<br />
-				GARANTIES<br />
-				CONDITIONS GÉNÉRALE DE VENTE<br />
+				<span class="titleFooter">NOS SERVICES</span>
+				<p>
+					<a href="#">LIVRAISON</a><br />
+					<a href="#">FINANCEMENT</a><br />
+					<a href="#">CATALOGUES</a><br />
+					<a href="#">GARANTIES</a><br />
+					<a href="#">CONDITIONS GÉNÉRALE DE VENTE</a><br />
+				</p>
 			</div>
-			<div class="col-lg-offset-1 col-lg-4">
-				A PROPOS DE NOUS<br /><br />
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor 
-				incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
-				exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute 
-				irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
-				nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
-				officia deserunt mollit anim id est laborum.
+			<div class="col-lg-offset-1 col-lg-4 aPropos">
+				<span class="titleFooter">A PROPOS DE NOUS</span>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor 
+					incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+					exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute 
+					irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+					nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
+					officia deserunt mollit anim id est laborum.
+				</p>
 			</div>
-			<div class="col-lg-offset-1 col-lg-3">
-				LOGO
+			<div class="col-lg-offset-1 col-lg-3 aPropos">
+				<span class="titleFooter">LOGO</span>
 			</div>
 		</div>
 	</div>
 	<div class="row mentions">
 		<div class="container">
 			<div class="col-lg-12">
-				Contact | Politique de confidentialité | Mentions légales | Plan du site
+				<a href="#">Contact</a> | <a href="#">Politique de confidentialité</a> | <a href="#">Mentions légales</a> | <a href="#">Plan du site</a>
 			</div>
 		</div>
 	</div>
