983d0dc62de7e81419fda10e42cf0bcd3df7af72
Loubna Fattouh <Loubna@MacBook-Pro-de-Loubna.local>
2016-02-01 14:58:56 +0100

    ajout image menu

--- resources/views/elements/ambiances.blade.php
+++ resources/views/elements/ambiances.blade.php
@@ -8,14 +8,50 @@
         <div class="content">
            <div class="row">
                 <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 ambiance1">
-                    <img src="http://placehold.it/540x500" alt="">
+                   <div class="ambiance-content">
+                       <img src="http://placehold.it/540x500" alt="">
+                       <div class="ambiance-text">
+                           <div class="ambiance-name">Ambiance</div>
+                           <div class="ambiance-desc">
+                               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad                            minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                           </div>
+                           <div class="ambiance-view">
+                               <button class="hdg-button-default">Voir l'ambiance</button>
+                           </div>                           
+                       </div>
+
+                   </div>
                 </div>
                 <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                    
                     <div class="row ambiance2">
-                        <img src="http://placehold.it/540x240" alt="">
+                       <div class="ambiance-content">
+                           <img src="http://placehold.it/540x240" alt="">
+                           <div class="ambiance-text">
+                               <div class="ambiance-name">Ambiance</div>
+                               <div class="ambiance-desc">
+                                   Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad                            minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                               </div>
+                               <div class="ambiance-view">
+                                   <button class="hdg-button-default">Voir l'ambiance</button>
+                               </div>                               
+                           </div>       
+                       </div>
                     </div>
+                    
                     <div class="row ambiance3">
-                        <img src="http://placehold.it/540x240" alt="">
+                       <div class="ambiance-content">
+                           <img src="http://placehold.it/540x240" alt="">
+                           <div class="ambiance-text">
+                               <div class="ambiance-name">Ambiance</div>
+                               <div class="ambiance-desc">
+                                   Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad                            minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                               </div>
+                               <div class="ambiance-view">
+                                   <button class="hdg-button-default">Voir l'ambiance</button>
+                               </div>                               
+                           </div>
+                       </div>
                     </div>
                 </div>
             </div>
