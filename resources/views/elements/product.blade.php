7d86fe3cdfd113cdaad512dd7c485fd7cfdb9926
nicoduflot <nico_duflot@yahoo.fr>
2016-02-01 13:46:42 +0100

    sélection produit bloc liste produit

--- resources/views/elements/product.blade.php
+++ resources/views/elements/product.blade.php
@@ -1,14 +1,14 @@
 @section('product')
 <div class="container">
 	<div class="row">
-		<div class="col-lg-3">
+		<div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
 			<div class="product-container">
 				<div class="call-add-basket">
 					<div class="call-product-page">
 						Afficher
 					</div>
 				</div>
-				<img src="http://placehold.it/250x250" />
+				<img src="http://placehold.it/250x250"  width="100%" />
 				<div class="caption">
 					<div class="desc-product">
 						<h3>Chaise design bois</h3>
@@ -23,12 +23,20 @@
 			</div>
 		</div>
 
-		<div class="col-lg-3">
+		<div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
 			<div class="product-container">
-				<img src="http://placehold.it/250x250" />
+				<div class="call-add-basket">
+					<div class="call-product-page">
+						Afficher
+					</div>
+				</div>
+				<img src="http://placehold.it/250x250"  width="100%" />
 				<div class="caption">
 					<div class="desc-product">
 						<h3>Chaise design bois</h3>
+					</div>
+					<div class="price-product">
+						333 €
 					</div>
 				</div>
 				<div class="bloc-add-basket">
@@ -36,22 +44,44 @@
 				</div>
 			</div>
 		</div>
-		<div class="col-lg-3">
+
+		<div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
 			<div class="product-container">
-				<img src="http://placehold.it/250x250" />
+				<div class="call-add-basket">
+					<div class="call-product-page">
+						Afficher
+					</div>
+				</div>
+				<img src="http://placehold.it/250x250"  width="100%" />
 				<div class="caption">
-					<h3>Chaise design bois</h3>
+					<div class="desc-product">
+						<h3>Chaise design bois</h3>
+					</div>
+					<div class="price-product">
+						333 €
+					</div>
 				</div>
 				<div class="bloc-add-basket">
 					<p><a href="#" class="btn btn-primary button-add-basket" role="button">Ajouter au panier</a></p>
 				</div>
 			</div>
 		</div>
-		<div class="col-lg-3">
+
+		<div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
 			<div class="product-container">
-				<img src="http://placehold.it/250x250" />
+				<div class="call-add-basket">
+					<div class="call-product-page">
+						Afficher
+					</div>
+				</div>
+				<img src="http://placehold.it/250x250"  width="100%" />
 				<div class="caption">
-					<h3>Chaise design bois</h3>
+					<div class="desc-product">
+						<h3>Chaise design bois</h3>
+					</div>
+					<div class="price-product">
+						333 €
+					</div>
 				</div>
 				<div class="bloc-add-basket">
 					<p><a href="#" class="btn btn-primary button-add-basket" role="button">Ajouter au panier</a></p>
