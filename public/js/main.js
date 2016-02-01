607c43150550b06fe368939f4bdaf74d89d89a3d
Loubna Fattouh <Loubna@MacBook-Pro-de-Loubna.local>
2016-02-01 16:39:48 +0100

    js sous menu

---
+++ public/js/main.js
@@ -0,0 +1,18 @@
+jQuery(document).ready(function () {
+    
+    //Manipulation des images du sous menu
+    
+    
+    
+    $(".salle-a-manger")
+    
+        
+    .mouseenter(function () {
+        $("#salle-a-manger").css("display", "block");  
+    })
+    .mouseleave(function (){
+        $("#salle-a-manger").css("display", "none");  
+    })
+    
+    
+});
