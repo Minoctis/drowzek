92c84c6568aac0f6b9085c1de768c10e90da2568
Loubna Fattouh <Loubna@MacBook-Pro-de-Loubna.local>
2016-02-05 22:57:03 +0100

    container nav

--- resources/views/elements/nav.blade.php
+++ resources/views/elements/nav.blade.php
@@ -2,7 +2,7 @@
 
 <nav class="navbar navbar-inverse navbar-fixed-top"id="nav" data-spy="affix" data-offset-top="500">
     <!-- Navbar Contents -->
-  <div class="container col-sm-12">
+  <div class="container">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header col-sm-2">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
