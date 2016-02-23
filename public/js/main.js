823e6114d3cd14d059242b20a67943fc87e09895
ftloubna <ftloubna@gmail.com>
2016-02-23 19:02:59 +0100

    css navigation

--- public/js/main.js
+++ public/js/main.js
@@ -3,15 +3,30 @@ jQuery(document).ready(function () {
     //Manipulation des images du sous menu
     
     
-    
+    // salle a manger
     $(".salle-a-manger")
-    
-        
     .mouseenter(function () {
         $("#salle-a-manger").css("display", "block");  
     })
     .mouseleave(function (){
         $("#salle-a-manger").css("display", "none");  
+    })
+
+    // Sejour
+    $(".sejour")    
+    .mouseenter(function () {
+        $("#sejour").css("display", "block");  
+    })
+    .mouseleave(function (){
+        $("#sejour").css("display", "none");  
+    })
+
+    $(".chambre") 
+    .mouseenter(function () {
+        $("#chambre").css("display", "block");  
+    })
+    .mouseleave(function (){
+        $("#chambre").css("display", "none");  
     })
     
     
