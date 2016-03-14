ddd31dc4eb5b03a26b61b8baeb8b4a50cf69ce01
nicoduflot <nico_duflot@yahoo.fr>
2016-03-14 11:05:45 +0100

    correction gitignore le repertoire product devient products

--- resources/views/pages/produit.blade.php
+++ resources/views/pages/produit.blade.php
@@ -5,8 +5,12 @@
 @section('content')
 
 <div class="row">
-	
+	<div class="col-lg-6 col-xs-12">
+		<img src="{{ asset('img/products/fauteuil-panier.jpg')}}" />
+	</div>
+	<div class="col-lg-6 col-xs-12">
 
+	</div>
 </div>
 
 @endsection
