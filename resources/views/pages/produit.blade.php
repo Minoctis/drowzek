30b996bc106af0c193122bf00dbd4c860b5d55aa
Loubna Fattouh <Loubna@MacBook-Pro-de-Loubna.local>
2016-03-15 22:47:46 +0100

    Modification de la structure de la fiche produit

--- resources/views/pages/produit.blade.php
+++ resources/views/pages/produit.blade.php
@@ -3,39 +3,40 @@
 @section('title', 'Page d\'accueil')
 
 @section('content')
-<div class="row size-carousel">
-	<div class="col-lg-6 col-xs-12 align-bottom">
-			<div class="carousel slide article-slide" id="myCarousel">
-		     	<div class="carousel-inner cont-slider">
-			        <div class="item active">
-			          <img class="img-products" src="{{ asset('img/products/fauteuil-panier.jpg')}}" width="100%" />
-			        </div>
-			        <div class="item">
-			          <img src="http://placehold.it/560x560/999999/cccccc">
-			        </div>
-			        <div class="item">
-			          <img src="http://placehold.it/560x560/dddddd/333333">
-			        </div>               
-		     	</div>
-		      <!-- Indicators -->
-		     	<ol class="carousel-indicators visible-lg visible-md">
-			        <li class="active" data-slide-to="0" data-target="#myCarousel">
-			          <img alt="" title="" src="http://placehold.it/120x44/cccccc/ffffff">
-			        </li>
-			        <li class="" data-slide-to="1" data-target="#myCarousel">
-			          <img alt="" title="" src="http://placehold.it/120x44/999999/cccccc">
-			        </li>
-			        <li class="" data-slide-to="2" data-target="#myCarousel">
-			          <img alt="" title="" src="http://placehold.it/120x44/dddddd/333333">
-			        </li>               
-		     	</ol>                 
-		    </div>
-	</div>
-	<div class="col-lg-6 col-xs-12 padding-product showborder">
-		<div class="row">
-			<div class="col-xs-12 showborder">
-				<h2>Craddle chair by Richard Clarkson</h2>
-				<h3>Richard Clarkson</h3>
+
+<div class="fiche-produit">
+	<div class="row size-carousel">
+		<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 align-bottom bloc-images" >
+				<div class="carousel slide article-slide" id="myCarousel">
+			     	<div class="carousel-inner cont-slider">
+				        <div class="item active">
+				          <img class="img-products" src="{{ asset('img/products/fauteuil-panier.jpg')}}" width="100%" />
+				        </div>
+				        <div class="item">
+				          <img src="http://placehold.it/560x560/999999/cccccc">
+				        </div>
+				        <div class="item">
+				          <img src="http://placehold.it/560x560/dddddd/333333">
+				        </div>               
+			     	</div>
+			      <!-- Indicators -->
+			     	<ol class="carousel-indicators visible-lg visible-md">
+				        <li class="active" data-slide-to="0" data-target="#myCarousel">
+				          <img alt="" title="" src="http://placehold.it/120x44/cccccc/ffffff">
+				        </li>
+				        <li class="" data-slide-to="1" data-target="#myCarousel">
+				          <img alt="" title="" src="http://placehold.it/120x44/999999/cccccc">
+				        </li>
+				        <li class="" data-slide-to="2" data-target="#myCarousel">
+				          <img alt="" title="" src="http://placehold.it/120x44/dddddd/333333">
+				        </li>               
+			     	</ol>                 
+			    </div>
+		</div>
+		<div class="container">
+			<div class="col-lg-6 col-lg-offset-6 col-md-6 col-md-offset-6 col-sm-12 col-xs-12 ">
+				<h2 class="product-title">Craddle chair by Richard Clarkson</h2>
+				<p>Description :</p>
 				<p>
 					Designée par Richard Clarkson, Grace Emmanual, Brodie Campbell, 
 					Jeremy Broker, Eamon Moore, Kahlivia Russell et Joya Boerrigter, 
@@ -47,105 +48,69 @@
 					et de tout intérieur. Plus d’infos sur le site de Richard Clarkson.
 				</p>
 				<p>
-					Dimensions :
+					Dimensions : 
 				</p>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-lg-6 col-xs-12">
-				<p>
-					Note des clients ayant acheté ce produit : 5/5 
-				</p>
-			</div>
-			<div class="col-lg-6 col-xs-12">
-				<p>
-					<a href="#">Afficher les avis</a>
-				</p>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-lg-6 col-xs-12">
-				<p>
-					Prix : 263 €
-				</p>
-			</div>
-			<div class="col-lg-6 col-xs-12">
-				<p><a href="#" class="btn btn-primary hdg-button-default button-add-basket" role="button">Ajouter au panier</a></p>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-xs-12">
+				<p class="reviews">
+					Note des clients ayant acheté ce produit : 5/5, <a href="#">Afficher les avis</a>
+				</p>			
 				<div class="row">
-					<div class="col-lg-12">
-						Partagez sur les réseaux sociaux
+					<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+						<p class="product-price">
+							Prix : <strong>263 €</strong>
+						</p>
+					</div>
+					<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+						<p><a href="#" class="btn btn-primary hdg-button-default button-add-basket" role="button">Ajouter au panier</a></p>
 					</div>
 				</div>	
-				<div class="row reseaux">
-					<div class="col-lg-2 col-md-4 col-sm-4 col-xs-4 social-icons">
-						<p>
-							<a ref="#">
-								<span style="color:#043861">
-									<i class="fa fa-facebook"></i><br />
-									Facebook
-								</span>
-							</a>
-						</p>
+				<div class="row">
+					<div class="col-xs-12">
+						Partagez sur les réseaux sociaux
+						<div class="social">
+							<div class="fb social-icon">
+								<a href="#">
+									<i class="fa fa-facebook"></i> <br>
+									<span>Facebooks</span>
+								</a>
+							</div>
+							<div class="twitter social-icon">
+								<a href="#">
+									<i class="fa fa-twitter"></i> <br>
+									<span>Twitter</span>
+								</a>
+							</div>
+							<div class="google-plus social-icon">
+								<a href="#">
+									<i class="fa fa-facebook"></i> <br>
+									<span>Google+</span>
+								</a>
+							</div>
+							<div class="instagram social-icon">
+								<a href="#">
+									<i class="fa fa-instagram"></i> <br>
+									<span>Instagram</span>
+								</a>
+							</div>
+							<div class="pinterest social-icon">
+								<a href="#">
+									<i class="fa fa-pinterest"></i> <br>
+									<span>Pinterest</span>
+								</a>
+							</div>
+							<div class="youtube social-icon">
+								<a href="#">
+									<i class="fa fa-youtube"></i> <br>
+									<span>Youtube</span>
+								</a>
+							</div>
+						</div>
 					</div>
-					<div class="col-lg-2 col-md-4 col-sm-4 col-xs-4 social-icons">
-						<p>
-							<a ref="#">
-								<span style="color:#307be1">
-									<i class="fa fa-twitter"></i><br />
-									Twitter
-								</span>
-							</a>
-						</p>
-					</div>
-					<div class="col-lg-2 col-md-4 col-sm-4 col-xs-4 social-icons">
-						<p>
-							<a ref="#">
-								<span style="color:#ca0814">
-									<i class="fa fa-google-plus"></i><br />
-									Google +
-								</span>
-							</a>
-						</p>
-					</div>
-					<div class="col-lg-2 col-md-4 col-sm-4 col-xs-4 social-icons">
-						<p>
-							<a ref="#">
-								<span style="color:#77400b">
-									<i class="fa fa-instagram"></i><br />
-									Instagram
-								</span>
-							</a>
-						</p>
-					</div>
-					<div class="col-lg-2 col-md-4 col-sm-4 col-xs-4 social-icons">
-						<p>
-							<a ref="#">
-								<span style="color:#cd2c2f">
-									<i class="fa fa-pinterest"></i><br />
-									Pinterest
-								</span>
-							</a>
-						</p>
-					</div>
-					<div class="col-lg-2 col-md-4 col-sm-4 col-xs-4 social-icons">
-						<p>
-							<a ref="#">
-								<span style="color:#650205">
-									<i class="fa fa-youtube"></i><br />
-									Youtube
-								<span>
-							</a>
-						</p>
-					</div>
-				</div>
-			</div>
+				</div>		
+			</div>		
 		</div>
 	</div>
 </div>
+
 <div class="container">
 	<div class="related-product">
 		<div class="row">
