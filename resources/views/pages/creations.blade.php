4aee35720dbce5b02adaecf9aff6e0cb8f408400
Constance-L <constance.lemaire59000@gmail.com>
2016-03-16 12:01:25 +0100

    modif page création, ajout de la bannière, reste à modifier le css et le responsive de celle ci

--- resources/views/pages/creations.blade.php
+++ resources/views/pages/creations.blade.php
@@ -4,14 +4,22 @@
 
 @section('content')
 
+<div class="creation">
+	<img src="http://placehold.it/1349x200" alt="">
+
 <div class="container">
+	
+	
+
 	<div class="row">
-	    @for ($i = 0; $i < 8; $i++)
+
+	    @for ($i = 0; $i < 12; $i++)
 
 	    	@include('elements.product')
 	    
 	    @endfor
 	</div>
+</div>
 </div>
 
 @endsection
