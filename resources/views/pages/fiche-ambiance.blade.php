3c041e68ea301a5d502d1eac1053b590e1367e54
nicoduflot <nico_duflot@yahoo.fr>
2016-03-30 10:22:01 +0200

    détection bug fiche ambiance

--- resources/views/pages/fiche-ambiance.blade.php
+++ resources/views/pages/fiche-ambiance.blade.php
@@ -8,7 +8,7 @@
 <div class="container">
 	<div class="fiche-ambiance">
 		<div class="row border-ambiance padding-header">
-			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 product-images">
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 product-images slider-ambiances">
 				<div class="align-bottom bloc-images">
 					<div class="carousel slide article-slide" id="myCarousel-ambiance">
 				     	<div class="carousel-inner cont-slider">
@@ -16,30 +16,30 @@
 					          <img src="http://placehold.it/1200x500/cccccc/ffffff">
 					        </div>
 					        <div class="item">
-					          <img src="http://placehold.it/1200x500/999999/cccccc">
+					          <img src="http://placehold.it/1200x400/999999/cccccc">
 					        </div>
 					        <div class="item">
-					          <img src="http://placehold.it/1200x500/dddddd/333333">
+					          <img src="http://placehold.it/1100x500/dddddd/333333">
 					        </div>               
 				     	</div>
-				      <!-- Indicators -->
-				     	<ol class="carousel-indicators visible-lg visible-md">
-					        <li class="active" data-slide-to="0" data-target="#myCarousel-ambiance">
-					          <img alt="" title="" src="http://placehold.it/120x44/cccccc/ffffff">
-					        </li>
-					        <li class="" data-slide-to="1" data-target="#myCarousel-ambiance">
-					          <img alt="" title="" src="http://placehold.it/120x44/999999/cccccc">
-					        </li>
-					        <li class="" data-slide-to="2" data-target="#myCarousel-ambiance">
-					          <img alt="" title="" src="http://placehold.it/120x44/dddddd/333333">
-					        </li>               
-				     	</ol>                 
+				    	<!-- Indicators -->
+				    	<ol class="carousel-indicators visible-lg visible-md">
+							<li class="active" data-slide-to="0" data-target="#myCarousel-ambiance">
+								<img alt="" title="" src="http://placehold.it/120x44/cccccc/ffffff">
+							</li>
+							<li class="" data-slide-to="1" data-target="#myCarousel-ambiance">
+								<img alt="" title="" src="http://placehold.it/120x44/999999/cccccc">
+							</li>
+							<li class="" data-slide-to="2" data-target="#myCarousel-ambiance">
+								<img alt="" title="" src="http://placehold.it/120x44/dddddd/333333">
+							</li>               
+						</ol>
 				    </div>
 				</div>
 			</div>
 		</div>
 	</div>
-	<div class="selected-product">
+	<section>
 			<div class="row">
 				@for ($i = 0; $i < 12; $i++)
 
@@ -48,6 +48,6 @@
 			    @endfor
 			</div>
 		</div>	
-	</div>
+	</section>
 </div>
 @endsection
