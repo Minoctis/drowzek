a8d410f19c82a38bea856dac695bb64bc403f78f
nicoduflot <nico_duflot@yahoo.fr>
2016-03-31 07:31:43 +0200

    début du tunnel, page adresse

--- resources/views/pages/checkout/adresses.blade.php
+++ resources/views/pages/checkout/adresses.blade.php
@@ -5,10 +5,48 @@
 @section('page-id', 'checkout-adresses')
 
 @section('content')
-<div class="container">
-	<div class="row">
-		<div class="fond-noir">
-			identification
+<div class="container show-border">
+	<div class="row" style="height:100px;">
+		<div class="col-lg-12">
+			<div class="fond-noir">
+				Identification
+			</div>
+			<div class="triangle-noir index-5">
+				
+			</div>
+			<div class="triangle-blanc decal-15 index-4">
+				
+			</div>
+			<div class="fond-noir decal-29 index-3">
+				Identification
+			</div>
+			<div class="triangle-noir index-5">
+				
+			</div>
+			<div class="triangle-blanc decal-15 index-4">
+				
+			</div>
+			<div class="fond-noir decal-29 index-3">
+				Identification
+			</div>
+			<div class="triangle-noir index-5">
+				
+			</div>
+			<div class="triangle-blanc decal-15 index-4">
+				
+			</div>
+			<div class="fond-noir decal-29 index-3">
+				Identification
+			</div>
+			<div class="triangle-noir index-5">
+				
+			</div>
+			<div class="triangle-blanc decal-15 index-4">
+				
+			</div>
+			<div class="fond-noir decal-29 index-3">
+				Identification
+			</div>
 		</div>
 	</div>
 </div>
