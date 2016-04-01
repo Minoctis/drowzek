bfced5866a28b317b1dcc5a78743fdf13aba58e6
nicoduflot <nico_duflot@yahoo.fr>
2016-04-01 16:09:47 +0200

    création élément mini panier pour le checkout pour l'intégration dynamique

--- resources/views/pages/checkout/adresses.blade.php
+++ resources/views/pages/checkout/adresses.blade.php
@@ -93,52 +93,10 @@
 
 			</div>
 			<div class="col-lg-5 left-border">
-				<div class="container-fluid">
-					<h4>Mon panier</h4>
-					<div class="row overflow-panier">
-						@for ($i = 0; $i < 8; $i++)
-							
-							@include('elements.checkout-product')
 
-						@endfor
-					</div>
-					<div class="row black-border">
-						<div class="row">
-							<div class="col-lg-6 right-content">
-								Total TTC:
-							</div>
-							<div class="col-lg-6 right-content">
-								1899 €
-							</div>
-						</div>
-						<div class="row">
-							<div class="col-lg-6 right-content">
-								Total hors taxes:
-							</div>
-							<div class="col-lg-6 right-content">
-								1500 €
-							</div>
-						</div>
-						<div class="row">
-							<div class="col-lg-6 right-content">
-								Total TVA 20%:
-							</div>
-							<div class="col-lg-6 right-content">
-								499 €
-							</div>
-						</div>
-					</div>
-					<div class="row black-border">
-						<div class="row">
-							<div class="col-lg-6 right-content">
-								Montant Total TTC:
-							</div>
-							<div class="col-lg-6 right-content">
-								1899 €
-							</div>
-						</div>
-					</div>
-				</div>
+				@include('elements.checkout-mini-panier')
+				
+				
 			</div>
 		</div>
 		<div class="row">
