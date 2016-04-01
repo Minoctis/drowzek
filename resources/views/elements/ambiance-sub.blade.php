e7d9ef886e4f50419dae310515cd3a63893eb1e8
Mathilde Drouin <mathilde.drouin59@gmail.com>
2016-04-01 16:53:02 +0200

    dynamisation liste ambiances

--- resources/views/elements/ambiance-sub.blade.php
+++ resources/views/elements/ambiance-sub.blade.php
@@ -1,14 +1,14 @@
 <div class="col-lg-6 padding-sub">
 	<div class="border-ambiance">
 		<div class="see-ambiance-sub">
-			<h2>Vintage</h2>
+			<h2>{{ $ambiance['nom'] }}</h2>
 			<p>
-				Retrouvez une gamme de produit pour une ambiance vintage
+				{{ $ambiance['description'] }}
 			</p>
 			<a class="hdg-button-small ">
 				Découvrir
 			</a>
 		</div>
-		<img class="img-products" src="http://placehold.it/1398x880/999999/cccccc" width="100%" />
+		<img class="img-products" src="{{ isset($ambiance['image']) ? asset('img/ambiances/'.$ambiance['img']['img_name']) : 'http://placehold.it/1398x880/999999/cccccc' }}" width="100%" />
 	</div>
 </div>
