2d0dac6b29a202f252828b2c3c4f9c159fcdd375
Mathilde Drouin <mathilde.drouin59@gmail.com>
2016-04-01 16:33:36 +0200

    element produit pour chaque cas (nouveauté, produit lié, même catégorie)

---
+++ resources/views/elements/new-product.php
@@ -0,0 +1,25 @@
+		<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+			<div class="product-container">
+				<div class="image-content">
+					<div class="call-add-basket">
+						<div class="call-product-page">
+							<a href="#">Afficher</a>
+						</div>
+					</div>
+					<div>
+						<img src="http://placehold.it/250x250"  width="100%" />
+					</div>
+				</div>
+				<div class="caption">
+					<div class="desc-product">
+						<h3 class="product-title-list">Mon Super Produit</h3>
+					</div>
+					<div class="price-product">
+						168 €
+					</div>
+				</div>
+				<div class="bloc-add-basket">
+					<p><a href="#" class="btn btn-primary hdg-button-default button-add-basket" role="button">Ajouter au panier</a></p>
+				</div>
+			</div>
+		</div>
