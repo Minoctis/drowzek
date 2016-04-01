06da8b13102a2c49eb2653d13c3aeb2c0434c14a
Loubna Fattouh <Loubna@MacBook-Pro-de-Loubna.local>
2016-04-01 14:16:19 +0200

    Petite modifs css

--- resources/views/pages/fiche-ambiance.blade.php
+++ resources/views/pages/fiche-ambiance.blade.php
@@ -13,26 +13,35 @@
 					<div class="carousel slide article-slide" id="myCarousel-ambiance">
 				     	<div class="carousel-inner cont-slider">
 					        <div class="item active">
-					          <img src="http://placehold.it/1200x500/cccccc/ffffff">
+					          	<img src="http://placehold.it/1200x500/cccccc/ffffff">
+
 					        </div>
 					        <div class="item">
-					          <img src="http://placehold.it/1200x400/999999/cccccc">
+					          	<img src="http://placehold.it/1200x400/999999/cccccc">
+
 					        </div>
 					        <div class="item">
-					          <img src="http://placehold.it/1100x500/dddddd/333333">
+					          	<img src="http://placehold.it/1100x500/dddddd/333333">
+
 					        </div>               
 				     	</div>
 				    	<!-- Indicators -->
 				    	<ol class="carousel-indicators visible-lg visible-md">
-							<li class="active" data-slide-to="0" data-target="#myCarousel-ambiance">
-								<img alt="" title="" src="http://placehold.it/120x44/cccccc/ffffff">
-							</li>
-							<li class="" data-slide-to="1" data-target="#myCarousel-ambiance">
-								<img alt="" title="" src="http://placehold.it/120x44/999999/cccccc">
-							</li>
-							<li class="" data-slide-to="2" data-target="#myCarousel-ambiance">
-								<img alt="" title="" src="http://placehold.it/120x44/dddddd/333333">
-							</li>               
+				    		<div class="ambiance">
+				    			
+				    		</div>
+				    		<div class="indicator">
+				    			<li class="active" data-slide-to="0" data-target="#myCarousel-ambiance">
+								<img alt="" title="" src="http://placehold.it/160x150/cccccc/ffffff">
+								</li>
+								<li class="" data-slide-to="1" data-target="#myCarousel-ambiance">
+									<img alt="" title="" src="http://placehold.it/170x150/999999/cccccc">
+								</li>
+								<li class="" data-slide-to="2" data-target="#myCarousel-ambiance">
+									<img alt="" title="" src="http://placehold.it/150x150/dddddd/333333">
+								</li>         
+				    		</div>
+							      
 						</ol>
 				    </div>
 				</div>
