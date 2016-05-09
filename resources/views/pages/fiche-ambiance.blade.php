37050521d6c21343f8ad9e368da2b47799763d25
Mathilde Drouin <mathilde.drouin59@gmail.com>
2016-05-09 15:08:51 +0200

    fix images ambiance < placeholder si aucune image

--- resources/views/pages/fiche-ambiance.blade.php
+++ resources/views/pages/fiche-ambiance.blade.php
@@ -19,6 +19,11 @@
 						<!-- Indicators -->
 						<div class="carousel-indicators-wrapper">
 							<ol class="carousel-indicators">
+								@if ($ambiance->images->count() === 0)
+									<li data-target="#myCarousel-ambiance" data-slide-to="0" class="active">
+										<img alt="" title="" src="http://placehold.it/170x150/999999/cccccc">
+									</li>
+								@endif
 								@foreach($ambiance->images as $index => $image)
 								<li data-target="#myCarousel-ambiance" data-slide-to="{{ $index }}"{{ $index === 0 ? ' class="active"' : '' }}>
 									<img alt="" title="" src="/img/ambiances/{{ $image->img_name }}">
@@ -30,6 +35,17 @@
 						<!-- slider -->
 				     	<div class="carousel-inner " role="listbox">
 				     		<!-- Item 1 -->
+							@if ($ambiance->images->count() === 0)
+								<div class="item active">
+									<div class="carousel-page">
+										<img src="http://placehold.it/1200x600/999999/cccccc" class="img-responsive" style="margin:0px auto;"  />
+									</div>
+									<div class="carousel-caption">
+										<h5>{{ $ambiance->nom }}</h5>
+										<p>{{ $ambiance->description }}</p>
+									</div>
+								</div>
+							@endif
 							@foreach($ambiance->images as $index => $image)
 					        <div class="item{{ $index === 0 ? ' active' : '' }}">
 								<div class="carousel-page">
