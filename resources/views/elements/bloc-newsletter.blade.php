1caf10a31c4de116df6bceebae8d5926db60b13d
Loubna Fattouh <Loubna@MacBook-Pro-de-Loubna.local>
2016-05-11 10:56:13 +0200

    newsletter liste produit

---
+++ resources/views/elements/bloc-newsletter.blade.php
@@ -0,0 +1,18 @@
+<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+    <div class="product-container">
+            <div class="product-thumbnail-image">
+                <div class="bloc-newsletter-liste">
+                    <h2 class="bloc-newsletter-titre">Newsletter</h2>
+                    <p>
+                        Offres privilège, coups de coeur, ambiances et collections :
+                        ne passez pas à côté de notre actualité !
+                    </p>
+                    <div class="form-group">
+                        <input type="text" name="newsletter">
+                        <a href="#" class="hdg-button-default">Ok</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
