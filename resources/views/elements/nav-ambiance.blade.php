99ddaa1d4152fb0c020af2c221553d7ad54860ff
Mathilde Drouin <mathilde.drouin59@gmail.com>
2016-05-11 11:49:45 +0200

    dynamisation navigation sur catégorie

--- resources/views/elements/nav-ambiance.blade.php
+++ resources/views/elements/nav-ambiance.blade.php
@@ -1,9 +1,7 @@
 <ol class="breadcrumb liste-creation">
-	<li><a href="#">Home</a></li>
-	<li><a href="#">Library</a></li>
-	<li><a href="#">Library</a></li>
-	<li><a href="#">Library</a></li>
-	<li><a href="#">Library</a></li>
-	<li><a href="#">Library</a></li>
-	<li><a href="#">Library</a></li>
+	@if ($categorie->children->count() !== 0)
+		@foreach($categorie->children as $categorie_enfant)
+	<li><a href="{{ route('creations', $categorie_enfant->slug) }}">{{ $categorie_enfant->nom }}</a>
+		@endforeach
+	@endif
 </ol>
