b6110c792ba192166b26e296eafd1f48092a9677
Loubna Fattouh <loubna@myconsulting.digital>
2016-07-05 09:46:32 +0200

    supression search dans le menu

--- resources/views/elements/nav.blade.php
+++ resources/views/elements/nav.blade.php
@@ -44,35 +44,12 @@
           </ul>
         </li>
       </ul>
-      <!--<form class="navbar-form navbar-left" role="search">
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="Search">
-        </div>
-        <button type="submit" class="btn btn-default">Submit</button>
-      </form>-->
+      
       <ul class="nav navbar-nav navbar-right">
         <li><a href="{{ route('compte::accueil') }}"><i class="fa fa-user fa-lg" style='float:left'></i> <span style="padding-left: 20px;" class='visible-xs'>  Mon compte</span> </a></li>
-        <li class="search-bloc" >
 
-          <form class="navbar-form" id="search-navbar-top" role="search"> 
-            <i class="fa fa-search fa-lg"></i>
-            <!-- <div class="form-group"> 
-              <input type="text" class="form-control" placeholder="Rechercher"> 
-            </div>  -->
-          </form>
-
-        </li>
         <li><a href="{{ route('panier') }}"><i class="fa fa-shopping-cart fa-lg"></i>  Panier</a></li>
-        <!--<li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li role="separator" class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-          </ul>
-        </li>-->
+
       </ul>
     </div><!-- /.navbar-collapse -->
   </div>
