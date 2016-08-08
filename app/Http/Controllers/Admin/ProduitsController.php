15b88c6cff1a7c9052fbf07284a879fccb2724ff
Mathilde Drouin <170590@supinfo.com>
2016-08-08 23:00:30 +0200

    liste des produits: dynamisation des categories et des ambiances

--- app/Http/Controllers/Admin/ProduitsController.php
+++ app/Http/Controllers/Admin/ProduitsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Models\Ambiance;
 use App\Models\Produit;
+use Debugbar;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -12,7 +13,7 @@ use App\Http\Controllers\Controller;
 class ProduitsController extends Controller
 {
     public function listeProduits() {
-        $produits = Produit::with('categorie')->get();
+        $produits = Produit::with('categorie.parent')->with('ambiances')->get();
         return view('pages.admin.produits.liste', ['produits' => $produits]);
     }
 }
--- resources/views/pages/admin/produits/liste.blade.php
+++ resources/views/pages/admin/produits/liste.blade.php
@@ -4,7 +4,7 @@
     <div class="container">
         <div class="row">
             <div class="table-responsive">
-                <table class="table">
+                <table class="table table-hover">
                     <thead>
                     <tr>
                         <th>Nom</th>
@@ -20,9 +20,9 @@
                     @foreach($produits as $produit)
                     <tr>
                         <td>{{$produit->nom}}</td>
-                        <td>Oui</td>
-                        <td><a href=""></a></td>
-                        <td><a href="">Ambiance 1</a></td>
+                        <td>{{ $produit->is_new ? 'Oui' : 'Non' }}</td>
+                        <td>@if($produit->categorie->parent->id)<a href="{{ route('admin::catalogue::categories::edit', $produit->categorie->parent->id) }}">{{ $produit->categorie->parent->nom }}@endif</a> // <a href="{{ route('admin::catalogue::categories::edit', $produit->categorie->id) }}">{{ $produit->categorie->nom }}</a></td>
+                        <td>@foreach($produit->ambiances as $ambiance)<a href="{{ route('admin::catalogue::ambiances::edit', $ambiance->id) }}">{{ $ambiance->nom }}</a>@endforeach</td>
                         <td>1</td>
                         <td><a href="" class="btn btn-default"><span class="glyphicon glyphicon-pencil"></span> Modifier</a></td>
                         <td><a href="" class="btn btn-default"><span class="glyphicon glyphicon-trash"></span> Supprimer</a></td>
