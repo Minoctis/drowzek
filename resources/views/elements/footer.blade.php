429aa6d4fe09904b59df8cdb0f2f18aff70755f2
nicoduflot <nico_duflot@yahoo.fr>
2016-08-24 15:06:23 +0200

    [SEO] alt et title sur les liens footer

--- resources/views/elements/footer.blade.php
+++ resources/views/elements/footer.blade.php
@@ -6,9 +6,9 @@
 				<div class="col-lg-3 col-md-4 col-sm-4 col-xs-12 bloc-footer">
 					<h3 class="title-footer">Nos services</h3>
 					<p>
-						<a href="#">Conditions générales de vente</a><br />
-						<a href="#">Livraison</a><br />
-						<a href="#">Garanties</a><br />
+						<a href="#" alt="Conditions générales de vente" title="Conditions générales de vente">Conditions générales de vente</a><br />
+						<a href="#" alt="Livraison" title="Livraison">Livraison</a><br />
+						<a href="#" alt="Garanties" title="Garanties">Garanties</a><br />
 					</p>
 				</div>
 				<div class="col-lg-6 col-md-4 col-sm-4 col-xs-12 bloc-footer">
@@ -29,7 +29,7 @@
 		<div class="container">
 			<div class="col-lg-12">
 				<p>
-					<a href="#">Contact</a> | <a href="#">Politique de confidentialité</a> | <a href="#">Mentions légales</a> | <a href="#">Plan du site</a>
+					<a href="#" alt="Nous contacter" title="Nous contacter">Contact</a> | <a href="#" alt="Politique de confidentialité" title="Politique de confidentialité">Politique de confidentialité</a> | <a href="#" alt="Mentions légales" title="Mentions légales">Mentions légales</a> | <a href="#" alt="Plan du site" title="Plan du site">Plan du site</a>
 				</p>
 			</div>
 		</div>
