823b6f0ef6fd7fc813334f97471730c6b1059c97
Loubna Fattouh <loubna@myconsulting.digital>
2016-09-03 10:46:24 +0200

    liste page static

---
+++ resources/views/pages/admin/pages/liste.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.admin')
+@section('title', 'Gestion des pages')
+@section('content')
+
+    <div class="row">
+        <div class="table-responsive">
+            <table class="table table-hover">
+                <thead>
+                <tr>
+                    <th>N°</th>
+                    <th>Titre</th>
+                    <th>Chemin</th>
+                    <th>Ordre</th>
+                    <th></th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($pages as $page)
+                    <tr>
+                        <td>{{$page->id}}</td>
+                        <td>{{$page->titre}}</td>
+                        <td>{{$page->slug}}</td>
+                        <td>{{$page->ordre}}</td>
+
+                        <td><a href="{{ route('admin::pages::details', $page->id) }}" class="btn btn-default"><span class="glyphicon glyphicon-eye-open"></span> Modifier</a></td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+@endsection
